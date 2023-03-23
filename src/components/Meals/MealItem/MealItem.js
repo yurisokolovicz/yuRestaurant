22d5e3c10513379e44b4d67791e51c3daf28b343
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-23 16:51:32 -0300

    Fixing Form Input IDs

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -12,7 +12,7 @@ const MealItem = props => {
                 <div className={styles.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm />
+                <MealItemForm id={props.id} />
             </div>
         </li>
     );
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -7,12 +7,12 @@ const MealItemForm = props => {
             <Input
                 label="Amount"
                 input={{
-                    id: 'amount',
+                    id: 'amount_' + props.id,
                     type: 'number',
                     min: '1',
                     max: '5',
                     step: '1',
-                    defaultvalue: '1'
+                    defaultValue: '1'
                 }}
             />
             <button>+ Add</button>
