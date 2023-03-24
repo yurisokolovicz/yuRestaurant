251a430795b14e34c89aa545b26eaa426e6484c4
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-24 11:47:57 -0300

    Using the Context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,23 @@
+import { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+    const cartCtx = useContext(CartContext);
+
+    // reduce method allow us to transform an array into a single value
+    const numberOfCarItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
     return (
         <button className={styles.button} onClick={props.onClick}>
             <span className={styles.icon}>
                 <CartIcon />
             </span>
             <span>Cart</span>
-            <span className={styles.badge}>3</span>
+            <span className={styles.badge}>{numberOfCarItems}</span>
         </button>
     );
 };
