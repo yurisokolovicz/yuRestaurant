2c7c7ae4bdf452db38a820bb4a9dcf9a537782e1
yurisokolovicz <yurisokolovicz@gmail.com>
2023-04-03 16:43:20 -0300

    Adding Form Validation

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,7 +1,17 @@
 import styles from './Checkout.module.css';
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
+
+// Helper function to check if a string is empty.
+const isEmpty = value => value.trim() === '';
+const isThreeChars = value => value.trim().length === 3;
 
 const Checkout = props => {
+    const [formInputsValidity, setFormInputsValidity] = useState({
+        card: true,
+        secureCode: true,
+        address: true
+    });
+
     const cardInputRef = useRef();
     const secureCodeInputRef = useRef();
     const addressInputRef = useRef();
@@ -12,6 +22,24 @@ const Checkout = props => {
         const enteredCard = cardInputRef.current.value;
         const enteredSecureCode = secureCodeInputRef.current.value;
         const enteredAddress = addressInputRef.current.value;
+
+        const enteredCardIsValid = !isEmpty(enteredCard);
+        const enteredSecureCodeIsValid = isThreeChars(enteredSecureCode);
+        const enteredAddressIsValid = !isEmpty(enteredAddress);
+
+        setFormInputsValidity({
+            card: enteredCardIsValid,
+            secureCode: enteredSecureCodeIsValid,
+            address: enteredAddressIsValid
+        });
+
+        const formIsValid = enteredCardIsValid && enteredSecureCodeIsValid && enteredAddressIsValid;
+
+        if (!formIsValid) {
+            return;
+        }
+
+        // Submit cart data
     };
 
     return (
@@ -19,17 +47,20 @@ const Checkout = props => {
             <div className={styles.control}>
                 <label htmlFor="text">PAYMENT</label>
             </div>
-            <div className={styles.control}>
+            <div className={`${styles.control} ${formInputsValidity.card ? '' : styles.invalid}`}>
                 <label htmlFor="card">Card number</label>
                 <input type="number" id="card" ref={cardInputRef} />
+                {!formInputsValidity.card && <p>Please enter a valid card number!</p>}
             </div>
-            <div className={styles.control}>
-                <label htmlFor="postal">Secure code</label>
-                <input type="number" id="postal" ref={secureCodeInputRef} />
+            <div className={`${styles.control} ${formInputsValidity.secureCode ? '' : styles.invalid}`}>
+                <label htmlFor="secure-code">Secure code</label>
+                <input type="number" id="secure-code" ref={secureCodeInputRef} />
+                {!formInputsValidity.card && <p>Please enter a valid secure code!</p>}
             </div>
-            <div className={styles.control}>
-                <label htmlFor="city">Address</label>
-                <input type="text" id="city" ref={addressInputRef} />
+            <div className={`${styles.control} ${formInputsValidity.address ? '' : styles.invalid}`}>
+                <label htmlFor="address">Address</label>
+                <input type="text" id="address" ref={addressInputRef} />
+                {!formInputsValidity.card && <p>Please enter a valid address!</p>}
             </div>
             <div className={styles.actions}>
                 <button type="button" onClick={props.onCancel}>
