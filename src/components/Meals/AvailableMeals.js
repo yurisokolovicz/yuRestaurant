d834d7c9cc431b86d4954c283a96f4d38a3a584a
yurisokolovicz <yurisokolovicz@gmail.com>
2023-04-03 15:00:35 -0300

    Fetching Meals via Http

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,61 @@
+import { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import styles from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Churrasco',
-        description: 'Meet with fries.',
-        price: 22.99
-    },
-    {
-        id: 'm2',
-        name: 'Rice with meat',
-        description: 'A gaucho specialty!',
-        price: 16.5
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty.',
-        price: 12.99
-    },
-    {
-        id: 'm4',
-        name: 'Pancakes',
-        description: 'Healthy & delicious!',
-        price: 18.99
-    }
-];
+// const DUMMY_MEALS = [
+//     {
+//         id: 'm1',
+//         name: 'Churrasco',
+//         description: 'Meet with fries.',
+//         price: 22.99
+//     },
+//     {
+//         id: 'm2',
+//         name: 'Rice with meat',
+//         description: 'A gaucho specialty!',
+//         price: 19.47
+//     },
+//     {
+//         id: 'm3',
+//         name: 'Barbecue Burger',
+//         description: 'American, raw, meaty.',
+//         price: 12.99
+//     },
+//     {
+//         id: 'm4',
+//         name: 'Pancakes',
+//         description: 'Healthy & delicious!',
+//         price: 18.99
+//     }
+// ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => (
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('https://react-http-f7e2d-default-rtdb.firebaseio.com/meals.json');
+            const responseData = await response.json();
+
+            const loadedMeals = [];
+
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price
+                });
+            }
+
+            console.log(responseData);
+            setMeals(loadedMeals);
+        };
+        fetchMeals();
+    }, []); // no dependencies so it runs only once when the component is rendered for the first time.
+
+    const mealsList = meals.map(meal => (
         <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />
     ));
 
