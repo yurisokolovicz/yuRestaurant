b19321e6d41b5f2c66ba0cf2e3227a667f3ad797
yurisokolovicz <yurisokolovicz@gmail.com>
2023-04-03 15:46:10 -0300

    Adding a Checkout Form

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,13 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import Modal from '../UI/Modal';
 import styles from './Cart.module.css';
 import CartContext from '../../store/cart-context';
 import CartItem from './CartItem';
+import Checkout from './Checkout';
 
 const Cart = props => {
+    const [isCheckout, setIsCheckout] = useState(false);
+
     const cartCtx = useContext(CartContext);
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -17,6 +20,10 @@ const Cart = props => {
 
     const cartItemRemoveHandler = id => {
         cartCtx.removeItem(id);
+    };
+
+    const orderHandler = () => {
+        setIsCheckout(true);
     };
 
     const cartItems = (
@@ -34,6 +41,19 @@ const Cart = props => {
         </ul>
     );
 
+    const modalActions = (
+        <div className={styles.actions}>
+            <button className={styles['button--alt']} onClick={props.onClose}>
+                Close
+            </button>
+            {hasItems && (
+                <button className={styles.button} onClick={orderHandler}>
+                    Order
+                </button>
+            )}
+        </div>
+    );
+
     return (
         <Modal onClose={props.onClose}>
             {cartItems}
@@ -41,12 +61,8 @@ const Cart = props => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            <div className={styles.actions}>
-                <button className={styles['button--alt']} onClick={props.onClose}>
-                    Close
-                </button>
-                {hasItems && <button className={styles.button}>Order</button>}
-            </div>
+            {isCheckout && <Checkout onCancel={props.onClose} />}
+            {!isCheckout && modalActions}
         </Modal>
     );
 };
